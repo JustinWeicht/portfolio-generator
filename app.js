5842fa212759bce24734c246552f4fc0fb314c33
Justin Weicht <justinweicht11@gmail.com>
2022-11-01 13:58:22 -0400

    setup inquirer

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-// const fs = require('fs');
 const inquirer = require('inquirer');
+// const fs = require('fs');
 // const generatePage = require('./src/page-template');
 
 // const pageHTML = generatePage(name, github);
